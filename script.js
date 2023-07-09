9616185b2a3058a5ef7b1fed436169dbdaaf2ad7
jazpal <you@example.com>
2023-07-09 08:40:57 -0700

    change variable name

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 "use strict";
 const body = document.querySelector("body");
-const clickMeBtn = document.querySelector(".btn");
+const colorChangerBtn = document.querySelector(".btn");
 const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-clickMeBtn.addEventListener("click", function () {
+colorChangerBtn.addEventListener("click", function () {
   let hexCol = "#";
   for (let i = 0; i < 6; i++) {
     let random = Math.floor(Math.random() * hexNumbers.length);
